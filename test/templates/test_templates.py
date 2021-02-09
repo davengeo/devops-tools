3575489d9c95a35400609755a11996d1b0f4ab8f
David Navarro <david.navarro@external.toyota-europe.com>
2021-02-09 13:21:01 +0100

    with templates

---
+++ test/templates/test_templates.py
@@ -0,0 +1,18 @@
+import os
+import sys
+
+import pytest
+from assertpy import assert_that
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+from devops.templates.templates import Templates  # noqa: E402
+from devops.common.config import Config  # noqa: E402
+
+config = Config(os.path.join(os.path.dirname(__file__), '../../app.ini'))
+
+
+@pytest.mark.integration
+def test_render_a_simple_template() -> None:
+    templates = Templates(config)
+    result = templates.render(template_name='hello_world', data={'mustache': 'Unit test'})
+    assert_that(result).is_equal_to('Hello, Unit test!')
